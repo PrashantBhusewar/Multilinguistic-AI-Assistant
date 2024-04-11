c414a2bd2f5986f984483ec8eebb3e09708fcd1b
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-11 22:45:57 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multilingual_assistant",
+    version="0.0.1",
+    author="Prashant",
+    author_email="prashantbhusewar1599@gmail.com",
+    packages=find_packages()
+)
